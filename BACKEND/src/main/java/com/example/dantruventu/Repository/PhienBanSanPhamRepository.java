9a0331cf206bf3d9858647a06052eef31f4954ea
Dai Nguyen <daintth03468@fpt.edu.vn>
2026-09-23 14:02:42 +0700

    Delete BACKEND/src/main/java/com/example/dantruventu/Repository/PhienBanSanPhamRepository.java
    Xóa file trùng PhienBanSanPhamRepository trong folder product

--- BACKEND/src/main/java/com/example/dantruventu/Repository/PhienBanSanPhamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dantruventu.Repository;
-
-import com.example.dantruventu.Entity.PhienBanSanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhienBanSanPhamRepository
-        extends JpaRepository<PhienBanSanPham, Long> {
-}
